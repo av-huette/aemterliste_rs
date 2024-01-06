4fae1dc614dbe61bd13daa56757419bf91d63535
Christopher Kaag <christopher.kaag@gmail.com>
2024-01-06 13:42:13 +0100

    format fix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker build -t n3phtys/aemterliste_rs:latest .
+docker build -t n3phtys/aemterliste_rs:latest . || exit 1
 docker save n3phtys/aemterliste_rs:latest > image.tar && scp -P 822 ./image.tar gruin@159.69.27.209:/tmp/image.tar && rm image.tar
 ssh -p 822 gruin@159.69.27.209 'cd /opt/reservierungen && docker load -i /tmp/image.tar && docker-compose up -d --no-deps'
